28618088a4c81eef11218aa03f85a99ab9484a86
skyroxas <skyroxas1521@gmail.com>
2017-09-22 18:45:49 +0800

    隱藏 seo 區塊

--- template-parts/breadcrumb-yoast.php
+++ template-parts/breadcrumb-yoast.php
@@ -1,7 +1,7 @@
 <?php
   if ( function_exists('yoast_breadcrumb') ) {
-    yoast_breadcrumb('<div id="breadcrumb" class="small">','</div>');
+    yoast_breadcrumb('<div id="breadcrumb" class="small text-center">','</div>');
   } else {
-    echo '<div id="breadcrumb" class="small"> breadcrumb is here </div>';
+    echo '<div id="breadcrumb" class="small text-center"> breadcrumb is here </div>';
   }
 ?>
